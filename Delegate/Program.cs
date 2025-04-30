1b9be725ce197effc69e9e6899b635722a196eb6
ukulsum <ukulsum1280@gmail.com>
2025-04-30 17:57:23 +0600

    delegates

---
+++ Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Delegate
+{
+    public delegate void Calculator(int x, int y);
+    public class Program
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(Add);
+            calc(20, 30);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MultiCast_Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MultiCast_Delegates
+{
+    public delegate void Calculator(int x, int y);
+    public class Program
+    {
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(Add);
+            calc += Mul;
+            calc(20, 30);
+            Console.ReadLine();
+        }
+    }
+}
